101180e67f43f566d300c574128074fc32e472ea
Santiago Paz <santiago.paz.1992@gmail.com>
2022-09-12 18:52:28 -0300

    Initial commit

---
+++ scripts/login.js
@@ -0,0 +1,12 @@
+// evaluar si hay un token para mandarlo directo a sus tareas
+window.addEventListener("load", function () {
+  /* -------------------------------------------------------------------------- */
+  /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
+  /* -------------------------------------------------------------------------- */
+  form.addEventListener("submit", function (event) {});
+
+  /* -------------------------------------------------------------------------- */
+  /*                     FUNCIÓN 2: Realizar el login [POST]                    */
+  /* -------------------------------------------------------------------------- */
+  function realizarLogin(user) {}
+});
---
+++ scripts/taks.js
@@ -0,0 +1,54 @@
+// SEGURIDAD: Si no se encuentra en localStorage info del usuario
+// no lo deja acceder a la página, redirigiendo al login inmediatamente.
+const jwt = localStorage.getItem("jwt");
+
+if (!jwt) {
+  // usamos el replace para no guardar en el historial la url anterior
+  location.replace("/");
+}
+
+/* ------ comienzan las funcionalidades una vez que carga el documento ------ */
+window.addEventListener("load", function () {
+  /* ---------------- variables globales y llamado a funciones ---------------- */
+
+  /* -------------------------------------------------------------------------- */
+  /*                          FUNCIÓN 1 - Cerrar sesión                         */
+  /* -------------------------------------------------------------------------- */
+
+  // btnCerrarSesion.addEventListener('click', function () {
+
+  // });
+
+  /* -------------------------------------------------------------------------- */
+  /*                 FUNCIÓN 2 - Obtener nombre de usuario [GET]                */
+  /* -------------------------------------------------------------------------- */
+
+  function obtenerNombreUsuario() {}
+
+  /* -------------------------------------------------------------------------- */
+  /*                 FUNCIÓN 3 - Obtener listado de tareas [GET]                */
+  /* -------------------------------------------------------------------------- */
+
+  function consultarTareas() {}
+
+  /* -------------------------------------------------------------------------- */
+  /*                    FUNCIÓN 4 - Crear nueva tarea [POST]                    */
+  /* -------------------------------------------------------------------------- */
+
+  formCrearTarea.addEventListener("submit", function (event) {});
+
+  /* -------------------------------------------------------------------------- */
+  /*                  FUNCIÓN 5 - Renderizar tareas en pantalla                 */
+  /* -------------------------------------------------------------------------- */
+  function renderizarTareas(listado) {}
+
+  /* -------------------------------------------------------------------------- */
+  /*                  FUNCIÓN 6 - Cambiar estado de tarea [PUT]                 */
+  /* -------------------------------------------------------------------------- */
+  function botonesCambioEstado() {}
+
+  /* -------------------------------------------------------------------------- */
+  /*                     FUNCIÓN 7 - Eliminar tarea [DELETE]                    */
+  /* -------------------------------------------------------------------------- */
+  function botonBorrarTarea() {}
+});
